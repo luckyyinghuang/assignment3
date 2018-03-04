fc69ffc9c58dd986f577b3d1f7143d81e6630c70
luckyyinghuang <ying.huang@ucdconnect.ie>
2018-03-04 10:21:21 +0000

    add

--- main.py
+++ main.py
@@ -3,15 +3,56 @@ Created on 27 Feb 2018
 
 @author: 25142
 '''
-from fileinput import filename
-from numpy.distutils.conv_template import file
-def main(filename,N):
-    lights=LightTester(N)
-    instructions=parse-file(filename())
-    for cmd in instructions:
-        lights.apply(cmd)
-        print("#occupied: ", light.count())
+""" Console Script for test"""
+import urllib.request
+help(urllib.request.urlopen)
+# make the request
+# the requests library is very useful and you will use it often:
+import requests
+# You will need to read files from the network too:
+def parseInput(input):
+        if input.startswith('http'):
+            # use urllib.request.urlopen(uri)
+            uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+            req = urllib.request.urlopen(uri)
+            buffer = req.read().decode('utf-8')
+                
+        else:
+            # use open(uri) 
+            uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+            r = requests.get(uri).text
+            print('\n'.join(r.split('\n')[:5]))
+            
+        return 
+            # use readlines to read a line a time
+            filename = "data/data.txt"
+            with open(filename) as f:
+                for line in f.readlines():
+                    # process line        
+                    values = line.strip().split()
+                    
+                
+            # read the whole file into a buffer
+            buffer = open(filename).read()
+            for line in buffer.split('\n'):
+                # process line
+              
+    
 
 
-if __name__ == '__main__':
-    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/test.py
+++ test/test.py
@@ -3,22 +3,45 @@ Created on 27 Feb 2018
 
 @author: 25142
 '''
-from numpy import *
-from itertools import count
+
+import sys
+sys.path.append('.')
+import pytest
+from click.testing import CliRunner
+import main
+import re
+help(re.match)                   
+pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+) \s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+result=pat.match("")
 class LightTest:
     lights=None
     def __init__(self,N):
-        self.lights=[[False]*size for _ in range(size)]
+        self.lights=[[False]*N for _ in range(N)]
     
     def apply(self,cmd):
         if cmd is 'turn on':
-            
+            for false in list turn true or turn from 1 to 0 ; 1 means false
+            print("turn on")
                  
         elif cmd is 'switch':
+            for 1 turns to 0
+            for 0 turns to 1
+            
+            print("switch")
         
         elif cmd is 'turn off':
+            for 0 turns on 1
+            print("turn off")
             
             
         
     def count(self):
+        count=0
+        for row in lights:
+            for light in row:
+                if(light==1):
+                    on=on+1
+        print("The number of lights on is ", on)
         return count
+    
+
